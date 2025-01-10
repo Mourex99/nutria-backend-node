f50171913edf6423f529e0bd63b70270b5df7798
Mateus Moura <mateusmoura.st@outlook.com>
2025-01-10 00:15:34 -0300

    firebase authentication implementation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
-# Cria o arquivo de rotas
-touch src/routes/passwordRoutes.js
+#!/bin/bash
 
-# Cria o arquivo de serviço
-touch src/services/passwordService.js
+# Baixar o BFG Repo-Cleaner
+echo "Baixando o BFG Repo-Cleaner..."
+wget https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar -O bfg.jar
 
-# Cria o arquivo de controlador
-touch src/controllers/passwordController.js
+# Navegar até o diretório do repositório
+cd /c/Users/Mourex/Documents/programming - folders/nutria-backend-node || exit
 
-# Atualiza o arquivo de modelo de usuário
-touch src/models/userModel.js
+# Usar o BFG para remover o arquivo sensível
+echo "Removendo o arquivo sensível do histórico do Git..."
+java -jar bfg.jar --delete-files 'src/config/nutria-fdfe7-firebase-adminsdk-wrr98-17f3cc57be.json'
+
+# Limpar e compactar o repositório
+echo "Limpando e compactando o repositório..."
+git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+
+# Fazer o push das mudanças para o repositório remoto
+echo "Fazendo o push das mudanças para o repositório remoto..."
+git push --force
+
+echo "Processo concluído com sucesso."
